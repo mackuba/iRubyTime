40d43226872f17beef526315af06ea74e52ab8aa
Jakub Suder <jakub.suder@gmail.com>
2009-04-30 20:10:13 +0200

    changed license to MIT

--- Classes/ActivityListController.m
+++ Classes/ActivityListController.m
@@ -1,11 +1,3 @@
-// ---------------------------------------------------------------------------------------
-// Copyright by Jakub Suder (2009)
-//
-// xBlip is free software: you can redistribute it and/or modify it under the terms of the
-// GNU General Public License as published by the Free Software Foundation, either version
-// 2 of the License, or (at your option) any later version.
-// ---------------------------------------------------------------------------------------
-
 #import "ActivityListController.h"
 #import "RubyTimeAppDelegate.h"
 #import "RubyTimeConnector.h"
--- Classes/LoginDialogController.h
+++ Classes/LoginDialogController.h
@@ -1,11 +1,3 @@
-// ---------------------------------------------------------------------------------------
-// Copyright by Jakub Suder (2009)
-//
-// xBlip is free software: you can redistribute it and/or modify it under the terms of the
-// GNU General Public License as published by the Free Software Foundation, either version
-// 2 of the License, or (at your option) any later version.
-// ---------------------------------------------------------------------------------------
-
 #import <UIKit/UIKit.h>
 
 @class RubyTimeConnector;
