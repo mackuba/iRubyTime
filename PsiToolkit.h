de7d08e4a8385f6fdd61e9c2e4f289abe20aceff
Jakub Suder <jakub.suder@gmail.com>
2010-05-05 16:13:44 +0200

    Squashed 'Lib/PsiToolkit/' content from commit eb3e94d
    git-subtree-dir: Lib/PsiToolkit
    git-subtree-split: eb3e94d8f8d1516af940701cd9a9afe3bd31b69d

---
+++ PsiToolkit.h
@@ -0,0 +1,16 @@
+// -------------------------------------------------------
+// PsiToolkit.h
+//
+// Copyright (c) 2010 Jakub Suder <jakub.suder@gmail.com>
+// Licensed under WTFPL license
+// -------------------------------------------------------
+
+#import "Macros.h"
+#import "FoundationExtensions.h"
+#import "PSIntArray.h"
+
+#if TARGET_OS_IPHONE
+#  import "UIExtensions.h"
+#else
+#  import "CocoaExtensions.h"
+#endif
